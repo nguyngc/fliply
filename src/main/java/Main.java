504056c71e6341248ef440e4e8b27c3c5cec0847
Thanh0201 <thanhho@metropolia.fi>
2026-02-26 21:51:12 +0200

    Move Controller UT, update HomeController

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,6 @@ import view.View;
 
 public class Main {
     public static void main(String[] args) {
-
-//        System.out.println("Starting JPA...");
-
-//        // Tạo EntityManager → Hibernate sẽ đọc persistence.xml
-//        EntityManager em = MariaDbJPAConnection.getInstance();
-//
-//        System.out.println("JPA started successfully!");
-
         View.launch(View.class);
     }
 }
